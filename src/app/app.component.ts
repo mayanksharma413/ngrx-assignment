bc45a29a2707e8917451b61ed331c2183e3538ee
mayanksharma4132 <mayanksharma4132@gmail.com>
2020-12-30 08:36:36 +0530

    project setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,16 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'ngrx-assignment';
+  source = [];
+  settings = {
+    columns: {
+      id: {
+        title: 'ID',
+      },
+    },
+  };
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Ng2SmartTableModule } from 'ng2-smart-table';
+import { AddUserComponent } from './add-user/add-user.component';
+import { AllUsersComponent } from './all-users/all-users.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, AddUserComponent, AllUsersComponent],
+  imports: [BrowserModule, AppRoutingModule, Ng2SmartTableModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
